02b7ccc7ae262318233973f1d7d5e3d47a30efd4
tonetii <toolscontrol@fenixaisolutions.com>
2026-05-18 10:52:54 +0200

    Navbar: user icon strokeWidth 1 -> 0.7 for thinner appearance

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -133,7 +133,7 @@ export default function Navbar() {
           {/* RIGHT: Account, Wishlist, Cart */}
           <div className="acne-nav-right">
             <Link href={accountHref} className="acne-right-icon" aria-label="Account">
-              <svg width="18" height="18" viewBox="0 0 16 16" fill="none" stroke="currentColor" strokeWidth="1" strokeLinejoin="round" strokeLinecap="round">
+              <svg width="18" height="18" viewBox="0 0 16 16" fill="none" stroke="currentColor" strokeWidth="0.7" strokeLinejoin="round" strokeLinecap="round">
                 <path d="M8 7C9.65685 7 11 5.65685 11 4C11 2.34315 9.65685 1 8 1C6.34315 1 5 2.34315 5 4C5 5.65685 6.34315 7 8 7Z" fill="none"/>
                 <path d="M14 12C14 10.3431 12.6569 9 11 9H5C3.34315 9 2 10.3431 2 12V15H14V12Z" fill="none"/>
               </svg>
